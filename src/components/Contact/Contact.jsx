3b453a1e41c00be17048c72e10e222395f803d70
Kacper <110663684+TheSKacper@users.noreply.github.com>
2024-01-29 19:31:20 +0100

    contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,42 +6,36 @@ const Contact = () => {
     <div className="container1">
       <h1 className="h1Contact">Skontaktuj się ze mną !</h1>
       <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Twoje imie
-          </label>
+        <div className="mb-3">
+          <label className="form-label">Twoje imie</label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             required
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Twój email
-          </label>
+        <div className="mb-3">
+          <label className="form-label">Twój email</label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="exampleInputPassword1"
             required
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Twoja wiadomość
-          </label>
+        <div className="mb-3">
+          <label className="form-label">Twoja wiadomość</label>
           <textarea
             type="text"
-            class="form-control"
+            className="form-control"
             id="exampleInputPassword1"
             rows={6}
             required
           />
         </div>
-        <button type="submit" class="btn">
+        <button type="submit" className="btn">
           Wyślij
         </button>
       </form>
